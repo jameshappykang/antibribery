6790a24b9b84087d89e22dcd5100c37333bb41e4
JamesKang7018 <etk1688@gmail.com>
2019-10-28 15:06:30 +0800

    改成SSL-465

--- mail/send.php
+++ mail/send.php
@@ -14,8 +14,8 @@ $mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers
 $mail->SMTPAuth = true; // Enable SMTP authentication
 $mail->Username = 'anti.bribery3@gmail.com'; // SMTP username
 $mail->Password = 'ANTI-BRI123'; // SMTP password
-$mail->SMTPSecure = 'tls'; // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587; // TCP port to connect to
+$mail->SMTPSecure = 'ssl'; // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to
 
 $mail->setFrom('anti.bribery3@gmail.com', '名字'); //寄件的Gmail
 $mail->addAddress('idcocode@gmail.com', '收件者名字'); // 收件的信箱
